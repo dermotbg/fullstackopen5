64d328e4c2a39f3d954e43e204263f1943110e04
Dermot Bateman <dermotbateman@gmail.com>
2023-09-26 13:53:34 +0300

    done: 5.1/5.2

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,68 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInAppUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedInAppUser', JSON.stringify(user)
+      )
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedInAppUser')
+    setUser(null)
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input type="text"
+            value={username}
+            name='Username'
+            onChange={({target}) => setUsername(target.value)} />
+          </div>
+          <div>
+            password
+            <input type="password"
+            value={password}
+            name='Password'
+            onChange={({target}) => setPassword(target.value)} />
+          </div>
+          <button type='Submit'>Login</button>
+        </form>
+      </div>
+  )}
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p> 
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+          )}
     </div>
   )
 }
