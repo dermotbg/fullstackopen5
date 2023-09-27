eeaa45b1a3ec29aba569735792bc5673c9671504
Dermot Bateman <dermotbateman@gmail.com>
2023-09-27 14:56:38 +0300

    done: 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+    
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+  
+  return (
+    <div className="blogStyle">
+        <div>
+          {blog.title} by {blog.author} 
+          <button onClick={toggleVisible}>{visible ? 'hide' : 'view'}</button>
+        </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes:  {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    </div>
+  ) 
+}
 
 export default Blog
