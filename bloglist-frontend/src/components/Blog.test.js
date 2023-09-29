49f5c6cfc2ab73b87fbc6e09b328b84e3c281cb8
Dermot Bateman <dermotbateman@gmail.com>
2023-09-29 08:38:02 +0300

    done: 5.13

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -9,10 +9,6 @@ beforeEach(() => {
     'loggedInAppUser',
     '{"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RfdXNlciIsImlkIjoiNjUwZDU2MjQwOGRmOWVmMDVmMTRmODk1IiwiaWF0IjoxNjk1ODc1MDQ5LCJleHAiOjE2OTU4Nzg2NDl9.PVr94illz2M3ua9GSQbVz3DFIFXYE8aq26C9HxZUZro","username":"test_user","name":"Test username"}'
   )
-})
-
-
-test('renders title/author, but not URL/likes', () => {
   const blog = {
     title: 'created in test',
     author: 'Tim Tester',
@@ -25,6 +21,22 @@ test('renders title/author, but not URL/likes', () => {
     }
   }
   const mockHandler = jest.fn()
+})
+
+
+test('renders title/author, but not URL/likes', (blog, mockHandler) => {
+  // const blog = {
+  //   title: 'created in test',
+  //   author: 'Tim Tester',
+  //   likes: 10,
+  //   url: 'www.shouldnotshow.com',
+  //   user: {
+  //     username: 'test_user',
+  //     name: 'Test Username',
+  //     id: '650d562408df9ef05f14f895'
+  //   }
+  // }
+  // const mockHandler = jest.fn()
 
   const { container } = render(<Blog blog={blog} updateBlogs={mockHandler} />)
   const title = container.querySelector('.title')
@@ -38,7 +50,9 @@ test('renders title/author, but not URL/likes', () => {
 
 })
 
-
+// test('URL & likes shown after view button clicked', async () => {
+//   const { container } = render(<Blog blog={blog} updateBlogs={mockHandler} />)
+// })
 
 afterEach(() => {
   window.localStorage.clear()
